c8030b8566cccfd115bd47b4ee7b79cff645dcbc
xvozt <xvoztuk@gmail.com>
2026-03-19 18:53:19 +0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    let command = buf.trim();
+    println!("{}: command not found", command);
+    io::stdout().flush().unwrap();
 }
